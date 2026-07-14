68e0220f2ee67b283b7d58c556a78fd621081e90
edward <edward_gio@hotmail.com>
2026-07-13 20:04:27 -0500

    1 - Variable

---
+++ OOPBearEdition/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPBearEdition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "MR Duff";
+
+            Console.WriteLine(name);
+        }
+    }
+}
